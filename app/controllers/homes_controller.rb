49658df75c57f1cc394a44a00ab4829ff6df617c
Hector Minaya <hminaya@gmail.com>
2013-01-08 14:47:45 -0400

    1st

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,14 @@
+class HomesController < ApplicationController
+  # GET /homes
+  # GET /homes.json
+  def index
+
+
+    respond_to do |format|
+      format.html # index.html.erb
+    end
+
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hm::Application.config.secret_token = 'd187b94fa9ed1d74a8ec24b9ce7342adbec4ea57617b976fd0c55abfb45892bdd40cab06209280a4d686da29a5d1fb48c2d9b73f417eacb92cbe0db7b59c03dc'
